41b87e54edabdccad71e0961d249e774420aa83a
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-03-03 18:18:28 -0300

    implementação da teoria da busca de curriculos

---
+++ src/main/java/com/jn/vis/commons/entities/VisEntityResumeNegativeted.java
@@ -0,0 +1,26 @@
+package com.jn.vis.commons.entities;
+
+import com.ccp.especifications.db.utils.CcpEntityField;
+import com.ccp.especifications.db.utils.CcpTimeOption;
+import com.jn.commons.entities.base.JnBaseEntity;
+
+public class VisEntityResumeNegativeted extends JnBaseEntity{
+	public VisEntityResumeNegativeted() {
+		super(false, CcpTimeOption.none, Fields.values());
+	}
+	public static enum Fields implements CcpEntityField{
+		company(true), email(true),
+		;
+		private final boolean primaryKey;
+
+		private Fields(boolean primaryKey) {
+			this.primaryKey = primaryKey;
+		}
+
+		@Override
+		public boolean isPrimaryKey() {
+			return this.primaryKey;
+		}
+
+	}
+}
