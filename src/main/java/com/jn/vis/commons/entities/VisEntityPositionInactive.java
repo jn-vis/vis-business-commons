0b6a14fe2434900a8c86156c16a1159417c928e2
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-03-07 23:04:42 -0300

    .

---
+++ src/main/java/com/jn/vis/commons/entities/VisEntityPositionInactive.java
@@ -0,0 +1,30 @@
+package com.jn.vis.commons.entities;
+
+import com.ccp.especifications.db.utils.CcpEntityField;
+import com.ccp.especifications.db.utils.CcpTimeOption;
+import com.jn.commons.entities.base.JnBaseEntity;
+
+public class VisEntityPositionInactive extends JnBaseEntity{
+	public VisEntityPositionInactive() {
+		super(false, CcpTimeOption.none, Fields.values());
+	}
+	public static enum Fields implements CcpEntityField{
+		title(true), email(true), description(false), contactChannel(false), expireDate(false), 
+		mandatorySkill(false), desiredSkill(false), ddd(false), seniority(false), pcd(false), 
+		disponibility(false), sortFields(false), frequency(false), channel(false), pj(false),  
+		btc(false), clt(false), experience(false), synonym(false), hash(false),
+		
+		;
+		private final boolean primaryKey;
+
+		private Fields(boolean primaryKey) {
+			this.primaryKey = primaryKey;
+		}
+
+		@Override
+		public boolean isPrimaryKey() {
+			return this.primaryKey;
+		}
+
+	}
+}
---
+++ src/main/java/com/jn/vis/commons/entities/VisEntityResumeInactive.java
@@ -0,0 +1,30 @@
+package com.jn.vis.commons.entities;
+
+import com.ccp.especifications.db.utils.CcpEntityField;
+import com.ccp.especifications.db.utils.CcpTimeOption;
+import com.jn.commons.entities.base.JnBaseEntity;
+
+public class VisEntityResumeInactive extends JnBaseEntity{
+	public VisEntityResumeInactive() {
+		super(false, CcpTimeOption.none, Fields.values());
+	}
+	public static enum Fields implements CcpEntityField{
+		ddd(false), email(true), disabilities(false), companiesNotAllowed(false), disponibility(false), 
+		observations(false), resumeBase64(false), resumeWords(false), resumeText(false),pj(false),
+		desiredJob(false), lastJob(false), btc(false), clt(false), experience(false), hash(false), 
+		synonym(false), lastUpdate(false)
+		,
+		;
+		private final boolean primaryKey;
+
+		private Fields(boolean primaryKey) {
+			this.primaryKey = primaryKey;
+		}
+
+		@Override
+		public boolean isPrimaryKey() {
+			return this.primaryKey;
+		}
+
+	}
+}
