e7b2ea1650cc4ab5ee37a814eb18cd6aa17a6d77
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-04-19 23:56:38 -0300

    .

--- src/main/java/com/jn/vis/commons/utils/VisTopics.java
+++ src/main/java/com/jn/vis/commons/utils/VisTopics.java
@@ -1,35 +1,6 @@
 package com.jn.vis.commons.utils;
 
-import com.ccp.especifications.mensageria.sender.CcpTopic;
-
-public enum VisTopics  implements CcpTopic {
-	saveResume {
-		public String getTopicName() {
-			return null;
-		}
-	},
-	savePosition {
-		public String getTopicName() {
-			return null;
-		}
-	},
-	inactivateResume {
-		public String getTopicName() {
-			return null;
-		}
-	},
-	inactivatePosition {
-		public String getTopicName() {
-			return null;
-		}
-	},
-	sendResumesToThisPosition {
-		@Override
-		public String getTopicName() {
-			return null;
-		}
-	}
-	;
-	public abstract String getTopicName();
+public enum VisTopics {
+	saveResume, savePosition, inactivateResume, inactivatePosition, sendResumesToThisPosition;
 
 }
