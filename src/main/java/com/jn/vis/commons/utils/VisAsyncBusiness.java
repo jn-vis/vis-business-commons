b9cf502f6c9b8f6ebec0360759929f3b70e10753
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-04-26 22:49:55 -0300

    .

---
+++ src/main/java/com/jn/vis/commons/utils/VisAsyncBusiness.java
@@ -0,0 +1,12 @@
+package com.jn.vis.commons.utils;
+
+public enum VisAsyncBusiness {
+	sendResumeToRecruiters, saveResumeFile,
+	 inactivateResume, inactivatePosition, 
+	 sendResumesToThisPositions, 
+	 positionGrouperNotifications, 
+	 resumeBucketGet, saveResume, 
+	 resumeGrouperNotifications
+	 ;
+
+}
--- src/main/java/com/jn/vis/commons/utils/VisTopics.java
+++
@@ -1,7 +0,0 @@
-package com.jn.vis.commons.utils;
-
-public enum VisTopics {
-	sendResumeToRecruiters, saveResumeFile,
-	 inactivateResume, inactivatePosition, sendResumesToThisPositions;
-
-}
