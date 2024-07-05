3be2abd018d36a5d703c02a97c2d34b6e3b19fbe
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-07-04 22:42:20 -0300

    .

--- src/main/java/com/jn/vis/commons/validations/JsonFieldsValidationsVisResume.java
+++ src/main/java/com/jn/vis/commons/validations/JsonFieldsValidationsVisResume.java
@@ -43,11 +43,10 @@ import com.ccp.validation.enums.YearValidations;
 
 		, allowedValues = {
 				@AllowedValues(rule = AllowedValuesValidations.arrayWithAllowedNumbers, fields = { "ddd" },//TODO COMO INTERNACIONALIZAR ISSO???
-						allowedValues = { "10", "61", "62", "64", "65", "66", "67", "82", "71", "73", "74", "75", "77",
-								"85", "88", "98", "99", "83", "81", "87", "86", "89", "84", "79", "68", "96", "92",
-								"97", "91", "93", "94", "69", "95", "63", "27", "28", "31", "32", "33", "34", "35",
-								"37", "38", "21", "22", "24", "11", "12", "13", "14", "15", "16", "17", "18", "19",
-								"41", "42", "43", "44", "45", "46", "51", "53", "54	", "55", "47", "48", "49" }),
+						allowedValues = {"10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "21", "22", "24", "27", "28", "31", "32", "33", "34", 
+								"35", "37", "38", "41", "42", "43", "44", "45", "46", "47", "48", "49", "51", "53", "54", "55", "61", "62", "63", 
+								"64", "65", "66", "67", "68", "69", "71", "73", "74", "75", "77", "79", "81", "82", "83", "84", "85", "86", "87", "88", 
+								"89", "91", "92", "93", "94", "95", "96", "97", "98", "99"}),
 				@AllowedValues(rule = AllowedValuesValidations.arrayWithAllowedTexts, fields = { "disabilities" },
 						allowedValues = { "física/motora", "intelectual/mental", "visual", "auditiva" })
 				//TODO COMO INTERNACIONALIZAR ISSO???
