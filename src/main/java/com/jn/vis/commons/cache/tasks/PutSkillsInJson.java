8180d0ee51112060ea6e1f7835308c895ecc6fb6
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-07-28 19:54:12 -0300

    .

---
+++ src/main/java/com/jn/vis/commons/cache/tasks/PutSkillsInJson.java
@@ -0,0 +1,68 @@
+package com.jn.vis.commons.cache.tasks;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import com.ccp.constantes.CcpConstants;
+import com.ccp.decorators.CcpJsonRepresentation;
+import com.ccp.decorators.CcpTextDecorator;
+import com.ccp.especifications.cache.CcpCacheDecorator;
+
+public class PutSkillsInJson implements Function<CcpJsonRepresentation, CcpJsonRepresentation>{
+
+	public final String fieldTextToRead;
+
+	public final String fieldToPut;
+	
+	public PutSkillsInJson(String fieldTextToRead, String fieldToPut) {
+		this.fieldTextToRead = fieldTextToRead;
+		this.fieldToPut = fieldToPut;
+	}
+
+	public CcpJsonRepresentation apply(CcpJsonRepresentation json) {
+		CcpCacheDecorator cache = new CcpCacheDecorator("skills");
+		List<CcpJsonRepresentation> resultAsList = cache.get(ReadSkillsFromDataBase.INSTANCE, 86400);
+		
+		CcpTextDecorator text = json.getAsTextDecorator(this.fieldTextToRead).sanitize();
+		List<CcpJsonRepresentation> allSkills = new ArrayList<>();
+		for (CcpJsonRepresentation skill : resultAsList) {
+		
+			Set<String> allSynonyms = 
+					skill.getAsJsonList("synonym")
+					.stream()
+					.map(synonym -> synonym.getAsString("skill"))
+					.collect(Collectors.toSet());
+			
+			Set<String> wordsThatWasFoundDirectlyInThisText = allSynonyms
+			.stream()
+			.filter(synonym -> text.contains(synonym))
+			.collect(Collectors.toSet());
+
+			boolean thisSkillWasNotFoundInThisText = wordsThatWasFoundDirectlyInThisText.isEmpty();
+			
+			if(thisSkillWasNotFoundInThisText) {
+				continue;
+			}
+			
+			List<String> parents = skill.getAsStringList("parent");
+			Set<String> wordsThatWasFoundAsSynonymInThisText = allSynonyms
+			.stream()
+			.filter(synonym -> wordsThatWasFoundDirectlyInThisText.contains(synonym) == false)
+			.collect(Collectors.toSet());
+			List<CcpJsonRepresentation> skills = wordsThatWasFoundDirectlyInThisText.stream().map(
+					word -> CcpConstants.EMPTY_JSON
+										.put("synonym", wordsThatWasFoundAsSynonymInThisText)
+										.put("parent", parents)
+										.put("skill", word)
+					).collect(Collectors.toList());
+			allSkills.addAll(skills);
+		}
+		
+		CcpJsonRepresentation jsonWithSkills = json.put(this.fieldToPut, allSkills);
+		return jsonWithSkills;
+	}
+
+}
--- src/main/java/com/jn/vis/commons/utils/VisCommonsUtils.java
+++ src/main/java/com/jn/vis/commons/utils/VisCommonsUtils.java
@@ -1,18 +1,9 @@
 package com.jn.vis.commons.utils;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import com.ccp.constantes.CcpConstants;
 import com.ccp.decorators.CcpJsonRepresentation;
 import com.ccp.decorators.CcpPropertiesDecorator;
 import com.ccp.decorators.CcpStringDecorator;
-import com.ccp.decorators.CcpTextDecorator;
-import com.ccp.especifications.cache.CcpCacheDecorator;
 import com.ccp.especifications.file.bucket.CcpFileBucketOperation;
-import com.jn.vis.commons.cache.tasks.ReadSkillsFromDataBase;
 import com.vis.commons.entities.VisEntityResume;
 
 public class VisCommonsUtils {
@@ -36,54 +27,6 @@ public class VisCommonsUtils {
 		boolean insuficientFunds = balanceValue <= totalCostToThisRecruiter;
 		
 		return insuficientFunds;
-	}
-	
-	public static CcpJsonRepresentation getJsonWithSkills(
-			CcpJsonRepresentation json
-			,String fieldTextToRead
-			,String fieldToPut
-			) {
-		
-		CcpCacheDecorator cache = new CcpCacheDecorator("skills");
-		List<CcpJsonRepresentation> resultAsList = cache.get(ReadSkillsFromDataBase.INSTANCE, 86400);
-		
-		CcpTextDecorator text = json.getAsTextDecorator(fieldTextToRead).sanitize();
-		List<CcpJsonRepresentation> allSkills = new ArrayList<>();
-		for (CcpJsonRepresentation skill : resultAsList) {
-		
-			Set<String> allSynonyms = 
-					skill.getAsJsonList("synonym")
-					.stream()
-					.map(synonym -> synonym.getAsString("skill"))
-					.collect(Collectors.toSet());
-			
-			Set<String> wordsThatWasFoundDirectlyInThisText = allSynonyms
-			.stream()
-			.filter(synonym -> text.contains(synonym))
-			.collect(Collectors.toSet());
-
-			boolean thisSkillWasNotFoundInThisText = wordsThatWasFoundDirectlyInThisText.isEmpty();
-			
-			if(thisSkillWasNotFoundInThisText) {
-				continue;
-			}
-			
-			List<String> parents = skill.getAsStringList("parent");
-			Set<String> wordsThatWasFoundAsSynonymInThisText = allSynonyms
-			.stream()
-			.filter(synonym -> wordsThatWasFoundDirectlyInThisText.contains(synonym) == false)
-			.collect(Collectors.toSet());
-			List<CcpJsonRepresentation> skills = wordsThatWasFoundDirectlyInThisText.stream().map(
-					word -> CcpConstants.EMPTY_JSON
-										.put("synonym", wordsThatWasFoundAsSynonymInThisText)
-										.put("parent", parents)
-										.put("skill", word)
-					).collect(Collectors.toList());
-			allSkills.addAll(skills);
-		}
-		
-		CcpJsonRepresentation jsonWithSkills = json.put(fieldToPut, allSkills);
-		return jsonWithSkills;
 	}
 	public static CcpJsonRepresentation getResumeFromBucket(CcpJsonRepresentation json) {
 		String email = json.getAsString("email");
