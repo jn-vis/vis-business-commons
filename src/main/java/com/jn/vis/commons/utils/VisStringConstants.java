7174f41f22cfd7e97cac6593f30cc234843351a5
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-12-31 19:48:40 -0300

    .

--- src/main/java/com/jn/vis/commons/utils/VisStringConstants.java
+++
@@ -1,9 +0,0 @@
-package com.jn.vis.commons.utils;
-
-public enum VisStringConstants {
-
-	ID_TO_LOAD_RESUME_ERROR_TEMPLATE_MESSAGE, ID_TO_LOAD_RESUME_SUCCESS_TEMPLATE_MESSAGE
-	,PORTUGUESE, originalEmail
-	
-	;
-}
