e998aefe5ef362ae8d7c324b627dafb03f9f3252
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2025-01-17 01:28:50 -0300

    .

--- src/main/java/com/vis/commons/entities/VisEntitySkill.java
+++ src/main/java/com/vis/commons/entities/VisEntitySkill.java
@@ -33,7 +33,7 @@ public class VisEntitySkill implements CcpEntityConfigurator {
 	
 	public List<CcpBulkItem> getFirstRecordsToInsert() {
 		List<CcpBulkItem> collect = new CcpStringDecorator("C:\\eclipse-workspaces\\ccp\\jn\\jn-dependency-chooser-documentation-and-junit-testing\\documentation\\skills\\synonyms.json")
-		.file().asJsonList().stream().map(json -> new CcpBulkItem(json, CcpEntityOperationType.create, ENTITY)).collect(Collectors.toList());
+		.file().asJsonList().stream().map(json -> ENTITY.toBulkItem(json, CcpEntityOperationType.create)).collect(Collectors.toList());
 		;
 		return collect;
 	}
